a7a601d770111240edf5bd14203b5a5a646246a6
J.D. Luke <jd@jdluke.com>
2021-10-13 06:00:20 -0400

    Added speed limiter fine control to left thumb stick.  Using this config with 3.15 PTU

--- bin/shove.sh
+++ bin/shove.sh
@@ -7,7 +7,7 @@ then
 	echo "You need to specify a control layout name, the same one used when saving the file in Star Citizen"
 else
 
-	REGION="LIVE"
+	REGION="PTU"
 	SC_PATH="/c/Program Files/Roberts Space Industries/StarCitizen/$REGION/USER/Client/0/Controls/Mappings"
 	NAME=$1
 	DESTINATION=data/controls
@@ -21,7 +21,7 @@ else
 	if [ -r $FILENAME ]
 	then
         	echo Copying exported control layout $NAME to SC $REGION
-		cp $FILENAME "$SCFILENAME" 
+		cp $DESTINATION/$FILENAME "$SCFILENAME" 
 	else
 		echo Unable to read $FILENAME
 	fi
